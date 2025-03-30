5da97fbe3c7b8173d368b818fc81e3d1237c798a
Phuoc1110 <phuoc11102004@gmail.com>
2025-03-30 21:16:53 +0700

    Product info

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ClientRoute from './routers/ClientRouter';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import ProductList from './components/ProductList';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import ProductInfo from './pages/ProductInfo';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppContextProvider from './context/AppContext';
+
 
 function App() {
   return (
-<<<<<<< HEAD
-    <AppContextProvider>
-      <Router>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route 
-              path="/" 
-              element={
-                <>
-                  <Banner />
-                  <ProductList />
-                </>
-              } 
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/product-info/:productId" element={<ProductInfo />} />
-          </Routes>
-          <Footer />
-        </div>
-      </Router>
-    </AppContextProvider>
-=======
     <ClientRoute />
->>>>>>> 462fe7d4a79762370932dbf85a7adc3d1b7a427f
   );
 }
 
--- frontend/src/routers/ClientRouter.js
+++ frontend/src/routers/ClientRouter.js
@@ -5,16 +5,22 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "../pages/Home";
 import AdminDashboard from "../pages/Admin/AdminDashboard";
 import Admin from "../pages/Admin/Admin";
+import ProductInfo from "../pages/ProductInfo";
+import AppContextProvider from "../context/AppContext";
 
 const ClientRoute = () => {
     return (
-        <Router>
+        <AppContextProvider>
+            <Router>
             <Routes>
                 <Route path="/home" element={<Home />} />
                 <Route path="/admin" element={<Admin />} />
-                <Route path="/" element={<Home />} /> {/* Add a route for the home page */}
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/login" element={<Login />} /> */}
+                <Route path="/product-info/:productId" element={<ProductInfo />} />
             </Routes>
         </Router>
+        </AppContextProvider>
     );
 };
 
