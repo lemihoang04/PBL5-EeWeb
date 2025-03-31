3f6a2065072fea1492d9150b318604193fda8f7b
Phuoc1110 <phuoc11102004@gmail.com>
2025-03-31 20:28:15 +0700

    Product

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,15 +1,23 @@
-import { createContext } from "react";
-import { iphones } from "../assets/images/assets";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-  const value = {
-    products: iphones,
-  };
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:5000/products")
+      .then((response) => {
+        console.log("Fetched Products:", response.data);
+        setProducts(response.data);
+      })
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+  
 
   return (
-    <AppContext.Provider value={value}>
+    <AppContext.Provider value={{ products }}>
       {props.children}
     </AppContext.Provider>
   );
--- frontend/src/routers/ClientRouter.js
+++ frontend/src/routers/ClientRouter.js
@@ -3,8 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 // Dummy components for demonstration
 import Home from "../pages/Home";
-import AdminDashboard from "../pages/Admin/AdminDashboard";
 import Admin from "../pages/Admin/Admin";
+import AppContextProvider from "../context/AppContext";
+import ProductInfo from "../pages/ProductInfo";
+import Cart from "../pages/Cart/Cart"
+import Login from "../pages/Login/Login"
+import Register from "../pages/Register/Register"
+
 
 const ClientRoute = () => {
     return (
@@ -14,12 +19,11 @@ const ClientRoute = () => {
                     <Route path="/home" element={<Home />} />
                     <Route path="/admin" element={<Admin />} />
                     <Route path="/" element={<Home />} />
-                    {/* <Route path="/login" element={<Login />} /> */}
-                    <Route path="/product-info" element={<ProductInfo />} /> {/* Add a route for product info */}
-                    <Route path="/cart" element={<Cart />} /> {/* Add a route for the cart page */}
-                    <Route path="/login" element={<Login />} /> {/* Add a route for the login page */}
-                    <Route path="/register" element={<Register />} /> {/* Add a route for the register page */}
+                    <Route path="/cart" element={<Cart />} /> 
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/register" element={<Register />} />
                     <Route path="/product-info/:productId" element={<ProductInfo />} />
+                    <Route path="/product-info" element={<ProductInfo />} /> 
                 </Routes>
             </Router>
         </AppContextProvider>
