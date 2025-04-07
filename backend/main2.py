577479b5b5111e6b288241e173e244280a2e3f4b
Phuoc1110 <phuoc11102004@gmail.com>
2025-04-07 20:19:18 +0700

    Database

--- backend/main2.py
+++ backend/main2.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, session
 from werkzeug.utils import secure_filename
 import os
-from backend.users import *
+from users import *
 from flask_cors import CORS
 
 app = Flask(__name__)
